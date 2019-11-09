e431751dbe2a2a31adb41bb50f0387ec11159e7b
Seojinseojin <1106laura@naver.com>
2019-11-09 19:09:15 +0900

    이렇게 하면 되나?

--- hackathonproject/settings.py
+++ hackathonproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'min1.apps.AppnameConfig',
+    'min1.apps.Min1Config',
 ]
 
 MIDDLEWARE = [
